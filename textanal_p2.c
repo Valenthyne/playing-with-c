f401f9eaf112050dd36b20a848c7c4b9821fc596
The Mysterious Stranger <52420930+Valenthyne@users.noreply.github.com>
2019-10-17 13:45:40 -0500

    Create textanal_p2.c

---
+++ textanal_p2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int length[29];
+
+int main()
+{
+
+    while (1 == 1) {
+
+        char str[1024];
+
+        printf("%s", "Please enter a string: ");
+        fgets(&str, 1024, stdin);
+
+        int word = 0;
+
+        for (int i = 0; str[i] != '\0'; i++) {
+
+            if (str[i] != ' ') {
+                word++;
+            } else {
+                if (word >= 29) {
+                    length[28]++;
+                } else {
+                    length[word - 1]++;
+                    word = 0;
+                }
+
+            }
+
+        }
+
+        length[word - 2]++;
+
+        puts("\Length | Occurrences\n-------------");
+        for (int i = 0; i < 28; i++) {
+            printf("%6d | %6d\n", i + 1, length[i]);
+        }
+
+        printf("%6s | %6d\n", "> 28", length[28]);
+
+        printf("%s\n", str);
+
+    }
+
+
+    return 0;
+}
