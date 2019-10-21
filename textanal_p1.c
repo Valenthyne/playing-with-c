a6f8a3aa3e5f4817046d8e3950d957e9e23e2591
The Mysterious Stranger <52420930+Valenthyne@users.noreply.github.com>
2019-10-21 18:02:50 -0500

    Update textanal_p1.c
    Slight updates, removed while loop, corrected last of letter verification

--- textanal_p1.c
+++ textanal_p1.c
@@ -5,28 +5,27 @@ int count[26];
 
 int main()
 {
+	// ASCII letter references
     // 65 - 90 is A to Z
     // 97 - 122 is a to z
 
-    while (1 == 1) {
+	char str[1024];
 
-        char str[100];
+	printf("%s", "Please enter a string: ");
+	fgets(&str, 1024, stdin);
 
-        printf("%s", "Please enter a string: ");
-        fgets(&str, 100, stdin);
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (isalpha(str[i])) {
+			int val = (int)str[i] - 1;
+			printf("%c is %d\n", str[i], (val % 32));
+			count[val % 32]++;
+		}
+	}
 
-        for (int i = 0; str[i] != '\0'; i++) {
-            int val = (int)str[i] - 1;
-            // printf("%c is %d", str[i], (val % 32));
-            count[val % 32]++;
-        }
+	puts("\nChar | Occurrences\n-------------");
+	for (int i = 0; i < 26; i++) {
+		printf("%4c | %4d\n", (char)(i + 65), count[i]);
+	}
 
-        puts("\nChar | Occurrences\n-------------");
-        for (int i = 0; i < 26; i++) {
-            printf("%4c | %4d\n", (char)(i + 65), count[i]);
-        }
-
-    }
-    
     return 0;
 }
