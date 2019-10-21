d17af026c009ebf70beecde9d095ced227d81aa7
The Mysterious Stranger <52420930+Valenthyne@users.noreply.github.com>
2019-10-21 18:13:45 -0500

    Update textanal_p1.c
    Added comments

--- textanal_p1.c
+++ textanal_p1.c
@@ -1,7 +1,14 @@
+/*
+	Solution for question 8.31 part a)
+	COMP 3150 -- Introduction to C/C++ Programming
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 1024
+#define LETTERS 26;
 
-int count[26];
+int count[LETTERS];
 
 int main()
 {
@@ -9,21 +16,30 @@ int main()
     // 65 - 90 is A to Z
     // 97 - 122 is a to z
 
-	char str[1024];
+	char str[SIZE];
 
 	printf("%s", "Please enter a string: ");
-	fgets(&str, 1024, stdin);
+	fgets(&str, SIZE, stdin);
 
+	// Begin cycling through the input string
 	for (int i = 0; str[i] != '\0'; i++) {
+		// Check if current character is an alphabetical letter
 		if (isalpha(str[i])) {
+			// Find the ASCII value of the letter and subtract by 1 for indexing
 			int val = (int)str[i] - 1;
-			printf("%c is %d\n", str[i], (val % 32));
+			
+			// Debug line
+			// printf("%c is %d\n", str[i], (val % 32));
+			
+			// Increment count of the specific letter in count array by one
+			// Using modulo two to relate indices of array to ASCII character values
 			count[val % 32]++;
 		}
 	}
 
+	// Print out table of all letters
 	puts("\nChar | Occurrences\n-------------");
-	for (int i = 0; i < 26; i++) {
+	for (int i = 0; i < LETTERS; i++) {
 		printf("%4c | %4d\n", (char)(i + 65), count[i]);
 	}
 
