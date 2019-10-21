7022b004f2bb4099e5c4fad59002c07efaa2ebed
The Mysterious Stranger <52420930+Valenthyne@users.noreply.github.com>
2019-10-21 18:16:25 -0500

    Update textanal_p2.c
    Added comments and reverted accidental commit where I replaced this with code for textanal_p1.c

--- textanal_p2.c
+++ textanal_p2.c
@@ -1,47 +1,51 @@
-/*
-	Solution for question 8.31 part a)
-	COMP 3150 -- Introduction to C/C++ Programming
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 1024
-#define LETTERS 26;
+#define MAX_LENGTH 29
 
-int count[LETTERS];
+int length[MAX_LENGTH];
 
 int main()
 {
-	// ASCII letter references
-    // 65 - 90 is A to Z
-    // 97 - 122 is a to z
 
 	char str[SIZE];
 
 	printf("%s", "Please enter a string: ");
 	fgets(&str, SIZE, stdin);
 
-	// Begin cycling through the input string
+	int word = 0;
+
+	// Begin cycling through input string
 	for (int i = 0; str[i] != '\0'; i++) {
-		// Check if current character is an alphabetical letter
-		if (isalpha(str[i])) {
-			// Find the ASCII value of the letter and subtract by 1 for indexing
-			int val = (int)str[i] - 1;
-			
-			// Debug line
-			// printf("%c is %d\n", str[i], (val % 32));
-			
-			// Increment count of the specific letter in count array by one
-			// Using modulo two to relate indices of array to ASCII character values
-			count[val % 32]++;
+
+		// Determine if current character is a space
+		if (str[i] != ' ') {
+			word++;
+		} else {
+			// Edge case where word might be longer than 29 characters
+			if (word >= MAX_LENGTH) {
+				length[MAX_LENGTH - 1]++;
+			} else {
+				// Otherwise, increment length array with the proper length value
+				length[word - 1]++;
+				// Reset length
+				word = 0;
+			}
+
 		}
+
 	}
 
-	// Print out table of all letters
-	puts("\nChar | Occurrences\n-------------");
-	for (int i = 0; i < LETTERS; i++) {
-		printf("%4c | %4d\n", (char)(i + 65), count[i]);
+	length[word - 2]++;
+
+	puts("\nLength | Occurrences\n-------------");
+	for (int i = 0; i < MAX_LENGTH - 1; i++) {
+		printf("%6d | %6d\n", i + 1, length[i]);
 	}
+
+	printf("%6s | %6d\n", "> 28", length[MAX_LENGTH - 1]);
+
+	printf("%s\n", str);
 
     return 0;
 }
