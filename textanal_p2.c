5e4d9f76ccce414a2599a980c73c2ef4f4229417
The Mysterious Stranger <52420930+Valenthyne@users.noreply.github.com>
2019-10-21 18:03:50 -0500

    Update textanal_p2.c
    Removed while loop, fixed issue with escape sequence again

--- textanal_p2.c
+++ textanal_p2.c
@@ -6,44 +6,39 @@ int length[29];
 int main()
 {
 
-    while (1 == 1) {
+	char str[1024];
 
-        char str[1024];
+	printf("%s", "Please enter a string: ");
+	fgets(&str, 1024, stdin);
 
-        printf("%s", "Please enter a string: ");
-        fgets(&str, 1024, stdin);
+	int word = 0;
 
-        int word = 0;
+	for (int i = 0; str[i] != '\0'; i++) {
 
-        for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] != ' ') {
+			word++;
+		} else {
+			if (word >= 29) {
+				length[28]++;
+			} else {
+				length[word - 1]++;
+				word = 0;
+			}
 
-            if (str[i] != ' ') {
-                word++;
-            } else {
-                if (word >= 29) {
-                    length[28]++;
-                } else {
-                    length[word - 1]++;
-                    word = 0;
-                }
+		}
 
-            }
+	}
 
-        }
+	length[word - 2]++;
 
-        length[word - 2]++;
+	puts("\nLength | Occurrences\n-------------");
+	for (int i = 0; i < 28; i++) {
+		printf("%6d | %6d\n", i + 1, length[i]);
+	}
 
-        puts("\nLength | Occurrences\n-------------");
-        for (int i = 0; i < 28; i++) {
-            printf("%6d | %6d\n", i + 1, length[i]);
-        }
+	printf("%6s | %6d\n", "> 28", length[28]);
 
-        printf("%6s | %6d\n", "> 28", length[28]);
-
-        printf("%s\n", str);
-
-    }
-
+	printf("%s\n", str);
 
     return 0;
 }
