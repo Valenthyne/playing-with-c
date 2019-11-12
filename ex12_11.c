6243a42c621809c519f1512ac45e29b571a64d4d
The Mysterious Stranger <52420930+Valenthyne@users.noreply.github.com>
2019-11-12 13:56:00 -0600

    Create ex12_11.c

---
+++ ex12_11.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 1024
+
+int check(char * str);
+
+struct stackNode {
+    char data;
+    struct stackNode * nextPtr;
+};
+
+typedef struct stackNode Node;
+typedef Node * NodePtr;
+
+// Stack function protoypes
+
+void push(NodePtr * top, int info);
+int pop(NodePtr * top);
+int isEmpty(NodePtr top);
+void printStack(NodePtr cur);
+
+int main()
+{
+    char str[MAX_SIZE];
+    printf("%s", "Please enter a string: ");
+    scanf("%[^\n]s", str);
+    int res = check(str);
+
+    if (res) {
+        printf("\n%s is a palindrome.\n", str);
+    } else {
+        printf("\n%s is not a palindrome.\n", str);
+    }
+
+    return 0;
+}
+
+int check(char * str) {
+
+    int len = strlen(str);
+
+    if (len == 1) {
+        return 1;
+    }
+
+    int mid = len / 2;
+
+    NodePtr stack = NULL;
+
+    for (int i = 0; i < mid; i++) {
+            push(&stack, str[i]);
+    }
+
+    if (len % 2 != 0) {
+        for (int i = mid + 1; i < len; i++) {
+            char ch = pop(&stack);
+            if (ch != str[i]) {
+                return 0;
+            }
+        }
+    } else {
+        for (int i = mid; i < len; i++) {
+            char ch = pop(&stack);
+            if (ch != str[i]) {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+
+}
+
+void push(NodePtr* top, int info) {
+
+    NodePtr newPtr = malloc(sizeof(Node));
+
+    if (newPtr != NULL) {
+        newPtr->data = info;
+        newPtr->nextPtr = *top;
+        *top = newPtr;
+    } else {
+        printf("\nMemory error.");
+    }
+
+}
+
+int pop(NodePtr * top) {
+
+    NodePtr temp = * top;
+    int val = (*top)->data;
+    *top = (*top)->nextPtr;
+    free(temp);
+    return val;
+}
+
+int isEmpty(NodePtr top) {
+    return top == NULL;
+}
+
+void printStack(NodePtr cur) {
+
+    printf("\n[ ");
+    while (cur != NULL) {
+        printf("%c ", cur->data);
+        cur = cur->nextPtr;
+    }
+    printf("]\n");
+}
